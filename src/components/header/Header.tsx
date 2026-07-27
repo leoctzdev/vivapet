1281f3721fe7201d7d180b4fc4d9c049aff592ff
Leonardo <leoctz.dev@gmail.com>
2026-07-27 16:54:14 -0300

    Feature: Header - Foi adicionado a estrutura base para a criação do cabeçalhp bem como o link referente a logo

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,21 @@
+export default function Header(){
+
+    return (
+
+        <header className="Header">
+
+            <div className="header-container">
+
+                <div className="logo">
+                    <Link href="/"> VivaPet </Link>
+                </div>
+
+                <nav className="menu">
+
+                </nav>
+
+            </div>
+
+        </header>
+    );
+}
