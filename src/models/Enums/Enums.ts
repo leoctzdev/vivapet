5adbe564f4debf06ee2ef515176f1870e5a279d0
Leonardo <leoctz.dev@gmail.com>
2026-08-03 16:47:50 -0300

    Iniciando a criação do pacote de domínio (models) com a classe Animal e seus respectivos enums

---
+++ src/models/Enums/Enums.ts
@@ -0,0 +1,19 @@
+export enum TipoAnimal{
+
+    GATO = "GATO",
+    CACHORRO = "CACHORRO"
+}
+
+export enum SexoAnimal{
+
+    MACHO = "MACHO",
+    FEMEA = "FEMEA"
+}
+
+export enum StatusAnimal{
+
+    ATIVO = "ATIVO",
+    EM_ADOCAO = "EM_ADOCAO",
+    ADOTADO = "ADOTADO",
+    FALECIDO = "FALECIDO"
+}
